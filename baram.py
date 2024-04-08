7b8e8edfb2d7c1df38cd4aa00f562690075daf4a
Todd W. Bucy <r3d91ll@bucy-medrano.me>
2024-04-08 13:36:38 -0500

    test 3 changes and data

--- baram.py
+++ baram.py
@@ -2,31 +2,49 @@ import subprocess
 import time
 import csv
 import datetime
+import argparse
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Baram - Advanced System Cooling Management Tool')
+parser.add_argument('--min-temp', type=int, default=50, help='Minimum temperature threshold (default: 50)')
+parser.add_argument('--max-temp', type=int, default=80, help='Maximum temperature threshold (default: 80)')
+parser.add_argument('--max-fan-speed', type=int, default=12000, help='Maximum fan speed (default: 12000)')
+parser.add_argument('--temp-drop', type=int, default=3, help='Temperature drop threshold for fan speed reduction (default: 3)')
+args = parser.parse_args()
 
 # Define thresholds for temperature
-MIN_TEMP = 50
-MAX_TEMP = 80
+MIN_TEMP = args.min_temp
+MAX_TEMP = args.max_temp
+
+# Define maximum fan speed
+MAX_FAN_SPEED = args.max_fan_speed
+
+# Define temperature drop threshold for fan speed reduction
+TEMP_DROP = args.temp_drop
 
 # Define PWM values
 MIN_PWM = 80
 MAX_PWM = 255
 
 # Define thresholds for temperature and corresponding PWM values
-TEMP_THRESHOLDS = [50, 60, 70, 75, 80]
-PWM_RANGES = [(80, 80), (80, 100), (100, 150), (150, 200), (200, 255), (255, 255)]
+TEMP_THRESHOLDS = [50, 60, 70, 72, 76]
+PWM_RANGES = [(80, 80), (80, 100), (100, 150), (150, 200), (200, MAX_PWM), (MAX_PWM, MAX_PWM)]
 
 # PWM control file and fan speed file
-PWM_FILE = "/sys/class/hwmon/hwmon2/pwm1"
+# need doc string here giving a breif explaination of how to adjust this
+PWM_FILE = "/sys/class/hwmon/hwmon2/pwm1" # Adjust based on your system
 FAN_SPEED_FILE = "/sys/class/hwmon/hwmon2/fan1_input"  # Adjust based on your system
-PWM_ENABLE_FILE = "/sys/class/hwmon/hwmon2/pwm1_enable"
+PWM_ENABLE_FILE = "/sys/class/hwmon/hwmon2/pwm1_enable" # Adjust based on your system
 
 # Log file
 LOG_FILE = "/var/log/gpu_temp.log"
 
-# Variables for tracking extended high power consumption
-HIGH_POWER_THRESHOLD = 200
-HIGH_POWER_DURATION = 300  # 5 minutes in seconds
-high_power_start_time = None
+# Variables for tracking temperature oscillation
+oscillation_count = 0
+oscillation_threshold = 3
+oscillation_interval = 30
+last_oscillation_time = None
+high_temp_reached = False
 
 def set_pwm_control_mode(mode):
     try:
@@ -86,31 +104,34 @@ while True:
     gpu_power = get_gpu_power()
     fan_speed = get_fan_speed()
 
-    if gpu_power >= HIGH_POWER_THRESHOLD:
-        if high_power_start_time is None:
-            high_power_start_time = time.time()
-    else:
-        high_power_start_time = None
+    if gpu_temp >= 76:
+        high_temp_reached = True
+        oscillation_count += 1
+        current_time = time.time()
+        if last_oscillation_time is None or current_time - last_oscillation_time >= oscillation_interval:
+            last_oscillation_time = current_time
+            oscillation_count = 1
+        if oscillation_count >= oscillation_threshold:
+            gpu_temp = 76
+    elif high_temp_reached and gpu_temp <= 76 - TEMP_DROP:
+        high_temp_reached = False
+        oscillation_count = 0
 
-    if high_power_start_time is not None and time.time() - high_power_start_time >= HIGH_POWER_DURATION:
-        # Maintain 8000 RPM until temperature drops below 50°C
-        if gpu_temp > MIN_TEMP:
-            pwm_value = PWM_RANGES[-2][0]  # Use the PWM value corresponding to 8000 RPM
+    pwm_value = MIN_PWM
+    for i in range(len(TEMP_THRESHOLDS)):
+        if i == 0:
+            if gpu_temp <= TEMP_THRESHOLDS[i]:
+                pwm_value = PWM_RANGES[i][0]
+                break
         else:
-            high_power_start_time = None
+            if TEMP_THRESHOLDS[i-1] < gpu_temp <= TEMP_THRESHOLDS[i]:
+                pwm_value = PWM_RANGES[i][0] + (gpu_temp - TEMP_THRESHOLDS[i-1]) * (PWM_RANGES[i][1] - PWM_RANGES[i][0]) // (TEMP_THRESHOLDS[i] - TEMP_THRESHOLDS[i-1])
+                break
     else:
-        pwm_value = MIN_PWM
-        for i in range(len(TEMP_THRESHOLDS)):
-            if i == 0:
-                if gpu_temp <= TEMP_THRESHOLDS[i]:
-                    pwm_value = PWM_RANGES[i][0]
-                    break
-            else:
-                if TEMP_THRESHOLDS[i-1] < gpu_temp <= TEMP_THRESHOLDS[i]:
-                    pwm_value = PWM_RANGES[i][0] + (gpu_temp - TEMP_THRESHOLDS[i-1]) * (PWM_RANGES[i][1] - PWM_RANGES[i][0]) // (TEMP_THRESHOLDS[i] - TEMP_THRESHOLDS[i-1])
-                    break
-        else:
-            pwm_value = PWM_RANGES[-1][1]
+        pwm_value = PWM_RANGES[-1][1]
+
+    if fan_speed > MAX_FAN_SPEED:
+        pwm_value = PWM_RANGES[-2][1]  # Set PWM value to the second-to-last range
 
     set_pwm_value(pwm_value)
 
