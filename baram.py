e9b4af5c3ed08d731497257a30e51d7daa8cb040
Todd W. Bucy <r3d91ll@bucy-medrano.me>
2024-04-08 02:16:58 -0500

    nightly backup

--- baram.py
+++ baram.py
@@ -5,11 +5,15 @@ import datetime
 
 # Define thresholds for temperature
 MIN_TEMP = 50
-MAX_TEMP = 85
+MAX_TEMP = 80
 
 # Define PWM values
-MIN_PWM = 0
+MIN_PWM = 80
 MAX_PWM = 255
+
+# Define thresholds for temperature and corresponding PWM values
+TEMP_THRESHOLDS = [50, 60, 70, 75, 80]
+PWM_RANGES = [(80, 80), (80, 100), (100, 150), (150, 200), (200, 255), (255, 255)]
 
 # PWM control file and fan speed file
 PWM_FILE = "/sys/class/hwmon/hwmon2/pwm1"
@@ -18,6 +22,11 @@ PWM_ENABLE_FILE = "/sys/class/hwmon/hwmon2/pwm1_enable"
 
 # Log file
 LOG_FILE = "/var/log/gpu_temp.log"
+
+# Variables for tracking extended high power consumption
+HIGH_POWER_THRESHOLD = 200
+HIGH_POWER_DURATION = 300  # 5 minutes in seconds
+high_power_start_time = None
 
 def set_pwm_control_mode(mode):
     try:
@@ -70,19 +79,38 @@ if current_mode != 1:
     set_pwm_control_mode(1)
 
 # Write CSV header to log file
-log_data(["date", "cpu_temp", "fan_speed", "pwm_value", "gpu_power"])
+log_data(["date", "gpu_temp", "fan_speed", "pwm_value", "gpu_power"])
 
 while True:
     gpu_temp = get_gpu_temp()
     gpu_power = get_gpu_power()
     fan_speed = get_fan_speed()
 
-    if gpu_temp <= MIN_TEMP:
-        pwm_value = MIN_PWM
-    elif gpu_temp >= MAX_TEMP:
-        pwm_value = MAX_PWM
+    if gpu_power >= HIGH_POWER_THRESHOLD:
+        if high_power_start_time is None:
+            high_power_start_time = time.time()
     else:
-        pwm_value = MIN_PWM + (gpu_temp - MIN_TEMP) * (MAX_PWM - MIN_PWM) // (MAX_TEMP - MIN_TEMP)
+        high_power_start_time = None
+
+    if high_power_start_time is not None and time.time() - high_power_start_time >= HIGH_POWER_DURATION:
+        # Maintain 8000 RPM until temperature drops below 50°C
+        if gpu_temp > MIN_TEMP:
+            pwm_value = PWM_RANGES[-2][0]  # Use the PWM value corresponding to 8000 RPM
+        else:
+            high_power_start_time = None
+    else:
+        pwm_value = MIN_PWM
+        for i in range(len(TEMP_THRESHOLDS)):
+            if i == 0:
+                if gpu_temp <= TEMP_THRESHOLDS[i]:
+                    pwm_value = PWM_RANGES[i][0]
+                    break
+            else:
+                if TEMP_THRESHOLDS[i-1] < gpu_temp <= TEMP_THRESHOLDS[i]:
+                    pwm_value = PWM_RANGES[i][0] + (gpu_temp - TEMP_THRESHOLDS[i-1]) * (PWM_RANGES[i][1] - PWM_RANGES[i][0]) // (TEMP_THRESHOLDS[i] - TEMP_THRESHOLDS[i-1])
+                    break
+        else:
+            pwm_value = PWM_RANGES[-1][1]
 
     set_pwm_value(pwm_value)
 
